989d070917eeb7bf76d7b195a1cf4e933c1904ca
Jesse_Hines <jhinesgu99@gmail.com>
2021-05-20 22:00:59 -0400

    edit margins

--- src/components/table.js
+++ src/components/table.js
@@ -1,142 +1,35 @@
 import React from 'react';
 import './table.css';
-// import Data from './data.json'
+import Data from './data.json'
 
 function Tmain() {
   return (
+            <div className="map-table-data">
+              { Data.map((dataDetail, index)=> {
+                return <table>
+                          <tbody>
+                            <table className="inner-table">
+                              <tr>
+                              <th scope="row"></th>
+                              <td>55</td>
+                              <td>55</td>
+                              <td>55</td>
+                              <td>55</td>
+                              <td>55</td>
+                              <td>55</td>
+                              <td>55</td>
+                              <td>55</td>
+                              <td>55</td>
+                              <td>55</td>
+                              </tr>
+                            </table>
+                          </tbody>
+                       </table>
+                
+              }) }
+            </div>
+    
 
-    <table>
-        <tbody>
-            <table className='inner-table'>
-              <tr>
-                <th scope="row"></th>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-              </tr>
-            </table>
-
-            <table className='inner-table'>
-              <tr>
-                <th scope="row">Whirlpool</th>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-              </tr>
-            </table>
-
-            <table className='inner-table'>
-              <tr>
-                <th scope="row">LG</th>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-              </tr>
-            </table>
-        
-            <table className='inner-table'>
-              <tr>
-                <th scope="row">GE</th>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-              </tr>
-            </table>
-
-            <table className='inner-table'>
-              <tr>
-                <th scope="row">Maytag</th>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-              </tr>
-            </table>
-
-            <table className='inner-table'>
-              <tr>
-                <th scope="row">Frigidaire</th>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-              </tr>
-            </table>
-
-            <table className='inner-table'>
-              <tr>
-                <th scope="row">KitchenAid</th>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-              </tr>
-            </table>
-
-            <table className='inner-table'>
-              <tr>
-                <th scope="row">Bosch</th>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-                <td>55</td>
-              </tr>
-            </table>
-
-          </tbody>
-        </table>
   );
 }
 
